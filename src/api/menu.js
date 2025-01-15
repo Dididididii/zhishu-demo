a1aa5af5cf49f7b2df4f07f9240801c4475e2496
Dididididii <1281627297@qq.com>
2025-01-15 21:28:00 +0800

    完成员工管理

--- src/api/menu.js
+++ src/api/menu.js
@@ -20,3 +20,45 @@ export const addEmployeeAPI = (data) => {
     data,
   });
 };
+
+/**
+ * 获取员工详情
+ * @param {*} id number
+ * @returns
+ */
+export const getEmployeeAPI = (id) => {
+  return request.get(`/park/sys/user/${id}`);
+};
+
+/**
+ * 编辑员工
+ * @param {*} data object
+ * @returns
+ */
+export const updateEmployeeAPI = (data) => {
+  return request.put(`/park/sys/user`, data);
+};
+
+/**
+ * 重置密码
+ * @param {*} id number
+ * @returns
+ */
+export const resetPwdAPI = (id) => {
+  return request({
+    url: "/park/sys/user/resetPwd",
+    method: "post",
+    data: {
+      id,
+    },
+  });
+};
+
+/**
+ * 删除员工
+ * @param {*} id number
+ * @returns
+ */
+export const delEmployeeAPI = (id) => {
+  return request.delete(`/park/sys/user/${id}`);
+};
--- src/views/System/Employee/index.vue
+++ src/views/System/Employee/index.vue
@@ -3,29 +3,29 @@
     <!-- 搜索区域 -->
     <div class="search-container">
       <div class="search-label">员工姓名：</div>
-      <el-input clearable placeholder="请输入内容" class="search-main" />
-      <el-button type="primary">查询</el-button>
+      <el-input v-model="params.name" clearable placeholder="请输入内容" class="search-main" />
+      <el-button type="primary" @click="searchUser">查询</el-button>
     </div>
     <div class="create-container">
       <el-button v-permission="'sys:user:add_edit'" type="primary" @click="addEmployee">添加员工</el-button>
     </div>
     <!-- 表格区域 -->
     <div class="table">
-      <el-table style="width: 100%" :data="employeeList">
+      <el-table v-loading="loading" style="width: 100%" :data="employeeList">
         <el-table-column type="index" label="序号" :index="indexMethod" />
         <el-table-column label="员工姓名" width="180" prop="name" />
         <el-table-column label="登录账号" width="180" prop="userName" />
         <el-table-column label="联系方式" width="120" prop="phonenumber" />
         <el-table-column label="角色" width="120" prop="roleName" />
         <el-table-column label="状态">
-          <template #default="scope">{{ scope.row.status }}</template>
+          <template #default="scope">{{ statusList[scope.row.status] }}</template>
         </el-table-column>
         <el-table-column label="添加时间" prop="createTime" />
         <el-table-column label="操作" fixed="right" width="120">
           <template #default="scope">
-            <el-button v-permission="'sys:user:add_edit'" size="mini" type="text">编辑</el-button>
-            <el-button v-permission="'sys:user:remove'" size="mini" type="text">删除</el-button>
-            <el-button v-permission="'sys:user:resetPwd'" size="mini" type="text">重置密码</el-button>
+            <el-button v-permission="'sys:user:add_edit'" size="mini" type="text" @click="editEenu(scope.row.id)">编辑</el-button>
+            <el-button v-permission="'sys:user:remove'" size="mini" type="text" @click="delEmployee(scope.row.id)">删除</el-button>
+            <el-button v-permission="'sys:user:resetPwd'" size="mini" type="text" @click="resetPwd(scope.row.id)">重置密码</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -34,7 +34,7 @@
       <el-pagination layout="total, prev, pager, next" :total="total" :page-size="2" @current-change="handleCurrentChange" />
     </div>
     <!-- 添加员工 -->
-    <el-dialog title="添加员工" width="480px" :visible.sync="dialogVisible" @close="closeDialog">
+    <el-dialog :title="addForm.id?'编辑员工':'添加员工'" width="480px" :visible.sync="dialogVisible" @close="closeDialog">
       <!-- 表单接口 -->
       <div class="form-container">
         <el-form ref="addForm" :model="addForm" :rules="addFormRules" label-width="80px">
@@ -69,19 +69,21 @@
 </template>
 
 <script>
-import { getEmployeeListAPI, addEmployeeAPI } from '@/api/menu'
+import { getEmployeeListAPI, addEmployeeAPI, getEmployeeAPI, updateEmployeeAPI, resetPwdAPI, delEmployeeAPI } from '@/api/menu'
 import { getSysRoleListAPI } from '@/api/system'
 
 export default {
   name: 'Employee',
   data() {
     return {
+      statusList: ['未启用', '启用 '],
       employeeList: [],
       params: {
         page: 1,
         pageSize: 2,
         name: null
       },
+      loading: false,
       total: 0,
       dialogVisible: false, // 控制弹框打开关闭
       roleList: [], // 角色列表
@@ -105,19 +107,88 @@ export default {
     this.getEmployeeList()
   },
   methods: {
+    // 删除员工
+    delEmployee(id) {
+      this.$confirm('确定要将此员工删除吗？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        await delEmployeeAPI(id)
+        this.getEmployeeList()
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+      })
+    },
+    // 重置密码
+    resetPwd(id) {
+      this.$confirm('确定要将密码重置为“123456”吗？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        await resetPwdAPI(id)
+        this.$message({
+          type: 'success',
+          message: '密码重置成功!'
+        })
+      })
+    },
+    // 编辑
+    async editEenu(id) {
+      const res1 = await getSysRoleListAPI()
+      this.roleList = res1.data
+      const res = await getEmployeeAPI(id)
+      // console.log(res)
+
+      this.addForm = res.data
+      this.dialogVisible = true
+    },
+    // 查询
+    searchUser() {
+      this.params.page = 1
+      this.getEmployeeList()
+    },
     submitForm() {
       this.$refs.addForm.validate(async valid => {
         if (!valid) return
         // 执行提交
-        await addEmployeeAPI(this.addForm)
+        if (this.addForm.id) {
+          // 编辑
+          // console.log(this.addForm)
+          const obj = this.addForm
+          delete obj.createTime
+          await updateEmployeeAPI(obj)
+          this.$message({
+            type: 'success',
+            message: '修改成功'
+          })
+        } else {
+          // 添加
+          await addEmployeeAPI(this.addForm)
+          this.$message({
+            type: 'success',
+            message: '添加成功'
+          })
+        }
+
         this.closeDialog()
         this.getEmployeeList()
       })
     },
     // 关闭弹框
     closeDialog() {
-      this.dialogVisible = false
+      this.addForm = {
+        name: '',
+        phonenumber: '',
+        roleId: '',
+        status: 1,
+        userName: ''
+      }
       this.$refs.addForm.resetFields()
+      this.dialogVisible = false
     },
     // 打开弹框
     async addEmployee() {
@@ -134,9 +205,11 @@ export default {
       this.getEmployeeList()
     },
     async getEmployeeList() {
+      this.loading = true
       const res = await getEmployeeListAPI(this.params)
       this.employeeList = res.data.rows
       this.total = res.data.total
+      this.loading = false
     }
   }
 }
